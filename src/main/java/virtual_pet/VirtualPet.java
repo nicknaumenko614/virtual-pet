949f110cd394b5845c1af9d9cebc07a66a0e861c
Nick Naumenko <carswithcharacter614@gmail.com>
2020-05-15 23:15:59 -0400

    Virtual Pet work in progress for instructors review

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -1,4 +1,91 @@
 package virtual_pet;
 
 public class VirtualPet {
+
+    private Integer hungerLevel;
+    private Integer thirstLevel;
+    private Integer crazyLevel;
+    private Integer bathroomBreak;
+
+    public VirtualPet(Integer hungerLevel, Integer thirstLevel, Integer crazyLevel, Integer bathroomBreak) {
+
+        this.hungerLevel = hungerLevel;
+        this.thirstLevel = thirstLevel;
+        this.crazyLevel = crazyLevel;
+        this.bathroomBreak = bathroomBreak;
+
+    }
+
+    public Integer getHungerLevel() {
+        return hungerLevel;
+    }
+
+    public void setHungerLevel(Integer hungerLevel) {
+        this.hungerLevel = hungerLevel;
+    }
+
+    public Integer getThirstLevel() {
+        return thirstLevel;
+    }
+
+    public void setThirstLevel(Integer thirstLevel) {
+        this.thirstLevel = thirstLevel;
+    }
+
+    public Integer getCrazyLevel() {
+        return crazyLevel;
+    }
+
+    public void setCrazyLevel(Integer crazyLevel) {
+        this.crazyLevel = crazyLevel;
+    }
+
+    public Integer getBathroomBreak() {
+        return bathroomBreak;
+    }
+
+    public void setBathroomBreak(Integer bathroomBreak) {
+        this.bathroomBreak = bathroomBreak;
+    }
+
+
+    public void tick() {
+        hungerLevel++;
+        thirstLevel++;
+        crazyLevel++;
+        bathroomBreak++;
+
+
+    }
+
+    public void feedPet() {
+        hungerLevel = hungerLevel - 2;
+        thirstLevel++;
+        crazyLevel++;
+        bathroomBreak++;
+        tick();
+    }
+
+    public void givePetWater() {
+        thirstLevel = thirstLevel - 2;
+        bathroomBreak++;
+        tick();
+
+    }
+
+    public void playWithPet() {
+        hungerLevel = hungerLevel + 1;
+        thirstLevel = thirstLevel + 1;
+        crazyLevel = crazyLevel - 2;
+        tick();
+    }
+
+    public void givePetBathroomBreak() {
+        bathroomBreak = bathroomBreak - 2;
+        hungerLevel++;
+        thirstLevel++;
+        tick();
+
+    }
+
 }
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,10 +1,57 @@
 package virtual_pet;
 
+import java.util.Scanner;
+
 public class VirtualPetApplication {
+    private static VirtualPet crazyGreyCat = new VirtualPet(5, 5, 5, 5);
 
     public static void main(String[] args) {
-        //Interact with a VirtualPet object in this method
+
+        System.out.println("Welcome to the Crazy Grey Cat game");
+        System.out.println("try to keep the cat happy so he doesn't run away!");
+        System.out.println("Cat will run away when any of his needs reach 10");
+        System.out.println(" ");
+        displayCatCurrentNeeds();
+
+        while (crazyGreyCat.getHungerLevel()<10 && crazyGreyCat.getThirstLevel()<10 && crazyGreyCat.getCrazyLevel()<10 && crazyGreyCat.getBathroomBreak()<10) {
+            System.out.println("What would you like to do? ");
+            System.out.println(" ");
+            System.out.println("To feed the cat, type 1");
+            System.out.println("To give cat water, type 2");
+            System.out.println("To calm the cat down, type 3");
+            System.out.println("To give cat a bathroom break, type 4");
+            System.out.println("To do nothing at this time, type 5");
+            System.out.println("To quit the game, type 6");
+
+            Scanner scanner1 = new Scanner(System.in);
+            int userSelection = Integer.valueOf(scanner1.next());
+            if (userSelection == 1) {
+                crazyGreyCat.feedPet();
+
+            } else if (userSelection == 2) {
+                crazyGreyCat.givePetWater();
+            } else if (userSelection == 3) {
+                crazyGreyCat.playWithPet();
+            } else if (userSelection == 4) {
+                crazyGreyCat.givePetBathroomBreak();
+            } else if (userSelection == 5) {
+                crazyGreyCat.tick();
+            } else if (userSelection == 6) {
+                System.out.println("Thank you for playing Crazy Grey Cat Game!");
+                System.exit(0);
+            }
+            displayCatCurrentNeeds();
+        }
+        System.out.println("Crazy Grey Cat ran away !");
+
 
     }
 
+    private static void displayCatCurrentNeeds() {
+        System.out.println("Cat's current hunger level is " + crazyGreyCat.getHungerLevel());
+        System.out.println("Cat's current thirst level is " + crazyGreyCat.getThirstLevel());
+        System.out.println("Cat's current crazy level is " + crazyGreyCat.getCrazyLevel());
+        System.out.println("Cat's current need for a bathroom break is " + crazyGreyCat.getBathroomBreak());
+    }
 }
+
