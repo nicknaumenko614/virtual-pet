b13ebf6afceb1b1e3dc99fca7215ac2155e1e34a
Nick Naumenko <carswithcharacter614@gmail.com>
2020-08-20 23:05:24 -0400

    redirecting from school repository to my own repository

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -73,4 +73,39 @@ public class VirtualPet {
 
     }
 
+    protected void displayGameObjective() {
+
+        System.out.println("Objective: try to keep the cat happy so he doesn't run away!");
+        System.out.println("Cat will run away when any of his needs reach 20");
+        System.out.println(" ");
+        displayCatCurrentNeeds();
+        printUserInstructions();
+    }
+
+
+    protected void displayCatCurrentNeeds() {
+        System.out.println("Cat's current hunger level is " + getHungerLevel());
+        System.out.println("Cat's current thirst level is " + getThirstLevel());
+        System.out.println("Cat's current crazy level is " + getCrazyLevel());
+        System.out.println("Cat's current need for a bathroom break is " + getBathroomBreak());
+    }
+
+    protected static void printUserInstructions() {
+
+        System.out.println("");
+        System.out.println("1 - To feed the cat");
+        System.out.println("2 - To give cat water");
+        System.out.println("3 - To calm the cat down");
+        System.out.println("4 - To give cat a bathroom break");
+        System.out.println("5 - To do nothing at this time");
+        System.out.println("6 - To quit the game");
+
+    }
+
+    protected boolean areNeedsTooHigh() {
+        return getHungerLevel() < 20 && getThirstLevel() < 20 && getCrazyLevel() < 20 && getBathroomBreak() < 20;
+
+
+    }
+
 }
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -8,9 +8,9 @@ public class VirtualPetApplication {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Crazy Grey Cat game");
-        displayGameObjective();
+        crazyGreyCat.displayGameObjective();
 
-        while (areNeedsTooHigh()) {
+        while (crazyGreyCat.areNeedsTooHigh()) {
 
 
             Scanner scanner1 = new Scanner(System.in);
@@ -30,12 +30,12 @@ public class VirtualPetApplication {
                 System.out.println("Thank you for playing Crazy Grey Cat Game!");
                 System.exit(0);
             } else if (userSelection == 0) {
-                printUserInstructions();
+                VirtualPet.printUserInstructions();
             }
 
             if (userSelection != 0) {
                 crazyGreyCat.tick();
-                displayCatCurrentNeeds();
+                crazyGreyCat.displayCatCurrentNeeds();
             }
 
 
@@ -46,39 +46,6 @@ public class VirtualPetApplication {
 
     }
 
-    private static void displayGameObjective() {
 
-        System.out.println("Objective: try to keep the cat happy so he doesn't run away!");
-        System.out.println("Cat will run away when any of his needs reach 20");
-        System.out.println(" ");
-        displayCatCurrentNeeds();
-        printUserInstructions();
-    }
-
-
-    private static void displayCatCurrentNeeds() {
-        System.out.println("Cat's current hunger level is " + crazyGreyCat.getHungerLevel());
-        System.out.println("Cat's current thirst level is " + crazyGreyCat.getThirstLevel());
-        System.out.println("Cat's current crazy level is " + crazyGreyCat.getCrazyLevel());
-        System.out.println("Cat's current need for a bathroom break is " + crazyGreyCat.getBathroomBreak());
-    }
-
-    private static void printUserInstructions() {
-
-        System.out.println("");
-        System.out.println("1 - To feed the cat");
-        System.out.println("2 - To give cat water");
-        System.out.println("3 - To calm the cat down");
-        System.out.println("4 - To give cat a bathroom break");
-        System.out.println("5 - To do nothing at this time");
-        System.out.println("6 - To quit the game");
-
-    }
-
-    private static boolean areNeedsTooHigh() {
-        return crazyGreyCat.getHungerLevel() < 20 && crazyGreyCat.getThirstLevel() < 20 && crazyGreyCat.getCrazyLevel() < 20 && crazyGreyCat.getBathroomBreak() < 20;
-
-
-    }
 }
 
